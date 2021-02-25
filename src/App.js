084ec34daae13136df91e7267ba02c7bce828052
LEARN Academy <>
2021-02-24 16:42:57 -0800

    added boxesand some logic and some css

--- src/App.js
+++ src/App.js
@@ -6,17 +6,42 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      firstPlayer: null,
+      secondPlayer: null,
     }
   }
+
+  //when you click the square it returns an x or an o
+handleGamePlay=() => {
+  const { squares, firstPlayer, secondPlayer }=
+    this.setState({ squares: "❌" })
+   {
+    this.setState({ squares: "〇" })
+  }
+}
+
+
 
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
-      </>
+        <div className="gameboard">
+        { this.state.squares.map((value, index) => {
+        return (
+          <Square
+          value= { value }
+          key= { index }
+          handleGamePlay= { this.handleGamePlay }
+        />
+      )
+    })}
+    </div>
+    </>
     )
   }
 }
+
+
 export default App
