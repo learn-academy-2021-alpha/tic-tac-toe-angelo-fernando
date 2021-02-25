9edd46b6ffb5d3bdc9355ba2d04aac72db73e72d
LEARN Academy <>
2021-02-25 14:05:18 -0800

    added two components, needs more componnents

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React, { Component } from 'react'
 import Square from './components/Square'
 import './App.css'
 
-class App extends Component{
+ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
       squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      firstPlayer: null,
-      secondPlayer: null,
+      winner: false,
+      draw: false,
+      actualPlayer: null,
     }
   }
+  
+  handleClick = () => {
+    this.props.handleGamePlay(this.props.index)
 
-  //when you click the square it returns an x or an o
-handleGamePlay=() => {
-  const { squares, firstPlayer, secondPlayer }=
-    this.setState({ squares: "❌" })
-   {
-    this.setState({ squares: "〇" })
-  }
-}
 
+  // when you click the square it returns an x or an o
+handleGamePlay = (index) => {
+  const { squares, winner, draw, actualPlayer } =
+  this.state
+  if(index === 0){
+    squares[index] = "❌"
+    this.setState({ squares: squares })
+  } else if(index !== 0){
+    squares[index] = "〇"
+    this.setState({ squares: squares })
+  } else {
+    squares[index] = "🤬"
+    this.setState({ squares: squares })
+}}
+//   (this.state.firstPlayer)
+//     this.setState({ squares: "❌" })
+//    {
+//     this.setState({ squares: "〇" })
+//   }
+// }
+//box 3 x 3
+//switching state from 0 to setstate of the emoji
+//determine winner
 
 
   render(){
